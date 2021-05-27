89e713d5180bfdabb739d6d7e869e5a17892e53c
Jung haesung <48352078+XxCtrlZxX@users.noreply.github.com>
2021-05-27 20:13:43 +0900

    player move

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Transform transform;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0.05f, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-0.05f , 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -0.05f , 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(0.05f, 0, 0));
+        }
+
+    }
+}
