e9d8d9baf303851a773b0c1b8986d8e067c99e9d
Jung haesung <48352078+XxCtrlZxX@users.noreply.github.com>
2021-06-15 12:46:14 +0900

    Create UIManager

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance => instance ??= FindObjectOfType<UIManager>();
+    private static UIManager instance;
+
+    private PlayerAction pa;
+
+    private void Awake()
+    {
+        pa = GameObject.FindWithTag("Player").GetComponent<PlayerAction>();
+    }
+
+    public void ButtonActivate() => pa.ActivateEquipment();
+    public void ButtonPick() => pa.PickEquipment();
+    public void ButtonCycle() => pa.CycleTools();
+}
